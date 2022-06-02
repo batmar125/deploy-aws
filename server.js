b82df4af20655ab43cb465359bfef259a2ac2300
batmar125 <104324570+batmar125@users.noreply.github.com>
2022-06-02 18:24:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,52 +1,45 @@
+const { application } = require('express')
 const express = require('express')
 const app = express()
 const port = 5000
 const puppeteer = require('puppeteer')
-let browser, page
-app.get('/run', async (req, res) => {
-    const minimal_args = [
-        '--autoplay-policy=user-gesture-required',
-        '--disable-background-networking',
-        '--disable-background-timer-throttling',
-        '--disable-backgrounding-occluded-windows',
-        '--disable-breakpad',
-        '--disable-client-side-phishing-detection',
-        '--disable-component-update',
-        '--disable-default-apps',
-        '--disable-dev-shm-usage',
-        '--disable-domain-reliability',
-        '--disable-extensions',
-        '--disable-features=AudioServiceOutOfProcess',
-        '--disable-hang-monitor',
-        '--disable-ipc-flooding-protection',
-        '--disable-notifications',
-        '--disable-offer-store-unmasked-wallet-cards',
-        '--disable-popup-blocking',
-        '--disable-print-preview',
-        '--disable-prompt-on-repost',
-        '--disable-renderer-backgrounding',
-        '--disable-setuid-sandbox',
-        '--disable-speech-api',
-        '--disable-sync',
-        '--hide-scrollbars',
-        '--ignore-gpu-blacklist',
-        '--metrics-recording-only',
-        '--mute-audio',
-        '--no-default-browser-check',
-        '--no-first-run',
-        '--no-pings',
-        '--no-sandbox',
-        '--no-zygote',
-        '--password-store=basic',
-        '--use-gl=swiftshader',
-        '--use-mock-keychain',
-      ];
-    browser = await puppeteer.launch({ headless: true, args: minimal_args, executablePath: '/usr/bin/chromium-browser' })
-    page = await browser.newPage()
-    console.log("Browser opened")
-    page.goto('https://blaze.com/pt/games/double')
-})
-
+const minimal_args = [
+    '--autoplay-policy=user-gesture-required',
+    '--disable-background-networking',
+    '--disable-background-timer-throttling',
+    '--disable-backgrounding-occluded-windows',
+    '--disable-breakpad',
+    '--disable-client-side-phishing-detection',
+    '--disable-component-update',
+    '--disable-default-apps',
+    '--disable-dev-shm-usage',
+    '--disable-domain-reliability',
+    '--disable-extensions',
+    '--disable-features=AudioServiceOutOfProcess',
+    '--disable-hang-monitor',
+    '--disable-ipc-flooding-protection',
+    '--disable-notifications',
+    '--disable-offer-store-unmasked-wallet-cards',
+    '--disable-popup-blocking',
+    '--disable-print-preview',
+    '--disable-prompt-on-repost',
+    '--disable-renderer-backgrounding',
+    '--disable-setuid-sandbox',
+    '--disable-speech-api',
+    '--disable-sync',
+    '--hide-scrollbars',
+    '--ignore-gpu-blacklist',
+    '--metrics-recording-only',
+    '--mute-audio',
+    '--no-default-browser-check',
+    '--no-first-run',
+    '--no-pings',
+    '--no-sandbox',
+    '--no-zygote',
+    '--password-store=basic',
+    '--use-gl=swiftshader',
+    '--use-mock-keychain',
+    ];
 function getdata () {
     return new Promise(async (resolve, reject) => {
         try {
@@ -57,9 +50,10 @@ function getdata () {
                     results.push({
                         id:  i + 1,
                         text: item.innerText,
+                        type: "Entrada e Número"
                     });
                 });
-                return results.slice(0, 20);
+                return results.slice(0 ,20);
               })
             return resolve(urls);
         } catch (e) {
@@ -69,11 +63,15 @@ function getdata () {
 }
             
 app.get('/api',async (req, res) => {
-    await getdata().then(await res.send(await getdata().then())).catch(console.log)
-
+    await getdata().then(await res.send(await getdata().then())).catch(console.log),
+    console.log("Request was made")
 })
 
-
-app.listen(port, () => {
-    console.log("You've started the server on port" + " " + port)
+app.listen(port, async () => {
+    console.log("You've started the server on port" + " " + port + "Your browser is opened. Please wait 1/2 seconds before going to next step.")
+    browser = await puppeteer.launch({ headless: true, args: minimal_args, executablePath: '/usr/bin/chromium-browser'})
+    page = await browser.newPage()
+    console.log("Browser opened")
+    page.goto('https://blaze.com/pt/games/double')
+    console.log("You entered the blazeURL/ Now you are able to run.")
 })
